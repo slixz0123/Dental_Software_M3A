f2e0acbefc6b857eb7b7ade0f23de4c11dd08549
Crisguamann <100295310+Crisguamann@users.noreply.github.com>
2022-02-25 23:18:36 -0500

    Cambio puerto en la conexion
    Se cambio el numero de puerto para prueba

--- Software_dental/src/View/Vista_crud_Tratamiento.java
+++ Software_dental/src/View/Vista_crud_Tratamiento.java
@@ -31,11 +31,11 @@ public class Vista_crud_Tratamiento extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 774, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 527, Short.MAX_VALUE)
         );
 
         pack();
